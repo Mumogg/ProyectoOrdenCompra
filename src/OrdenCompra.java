95770310e788cbcd7314f7b05a0db2c6140d491a
Sebastian Murillo <104529023+Mumogg@users.noreply.github.com>
2024-03-13 20:44:21 -0600

    first

---
+++ src/OrdenCompra.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class OrdenCompra {
+
+    private int id;
+    private String desc;
+    private Date fecha;
+    private Cliente cliente;
+    private String Productos[];
+}
